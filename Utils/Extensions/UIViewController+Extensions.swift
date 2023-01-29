6a698971ae0361c118229cef967db934bd003960
jesusmarzor <jesusmarzor28@gmail.com>
2023-01-30 00:35:17 +0100

    Change: loader style

--- Utils/Extensions/UIViewController+Extensions.swift
+++ Utils/Extensions/UIViewController+Extensions.swift
@@ -9,7 +9,7 @@ extension UIViewController {
     func showLoader() {
         let spinnerView = UIView(frame: self.view.frame)
         spinnerView.backgroundColor = UIColor.init(red: 0.5, green: 0.5, blue: 0.5, alpha: 0.5)
-        let ai = UIActivityIndicatorView(style: UIActivityIndicatorView.Style.large)
+        let ai = UIActivityIndicatorView(style: .large)
         ai.startAnimating()
         ai.center = spinnerView.center
         
